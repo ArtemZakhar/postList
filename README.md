e57d7107f8955f378c7f8d2cf06d3cdaa08da9ae
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-17 09:42:19 +0300

    deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+Project written using Vue, Bulma, Pinja.
+
+[DEMO LINK](https://artemzakhar.github.io/vue_list_of_posts/)
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue_list_of_posts',
+  base: '/postList',
   plugins: [vue()],
   resolve: {
     alias: {
